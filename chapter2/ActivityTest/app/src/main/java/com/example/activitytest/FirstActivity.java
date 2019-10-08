b9514ab0229a683a2dfe09bb20149a1aff06b1b8
jianwei_lee <2017118122@stu.hstc.edu.cn>
2019-10-08 09:29:21 +0800

    活动启动模式standard

--- chapter2/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
+++ chapter2/ActivityTest/app/src/main/java/com/example/activitytest/FirstActivity.java
@@ -3,7 +3,9 @@ package com.example.activitytest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -35,12 +37,14 @@ public class FirstActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d("FirstActivity",this.toString());
         setContentView(R.layout.first_layout);
         Button button1 = (Button) findViewById(R.id.button_1);
         button1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Toast.makeText(FirstActivity.this, "You clicked Button 1", Toast.LENGTH_SHORT).show();
+                Intent intent = new Intent(FirstActivity.this,FirstActivity.class);
+                startActivity(intent);
             }
         });
     }
